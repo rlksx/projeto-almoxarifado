bffd04fec16c9f5be86b890ab59ff58f1f326e6f
Ryan Gabriel <ryan.bromati@hotmail.com>
2023-06-21 18:16:16 +0000

    builder added in the almoxarife

--- src/Almoxarifado.Test/UnitTests/AlmoxarifeTest.cs
+++ src/Almoxarifado.Test/UnitTests/AlmoxarifeTest.cs
@@ -3,9 +3,13 @@ namespace Almoxarifado.Test;
 public class AlmoxarifeTest
 {
    private static Almoxarife _almoxarife;
+   private static Produto _produto;
 
-   public AlmoxarifeTest()
-      =>  _almoxarife = new Almoxarife("nome", "senha", 470000, 020302);
+    public AlmoxarifeTest()
+    {
+      _almoxarife = AlmoxarifeBuilder.Novo().Criar();
+      _produto = ProdutoBuilder.Novo().Criar();
+    } 
    
 
    [Theory]
@@ -13,7 +17,7 @@ public class AlmoxarifeTest
    [InlineData(-1)]
    public void dado_um_novo_almoxarife_cpf_deve_ser_maior_que_0(int _cpf)
    {
-      Assert.Throws<ArgumentException>(() => new Almoxarife("nome", "123456", 47000, _cpf))
+        Assert.Throws<ArgumentException>(() => AlmoxarifeBuilder.Novo().ComCpf(_cpf).Criar())
             .Message.Equals("Cpf invalido");
    }
 
@@ -23,7 +27,7 @@ public class AlmoxarifeTest
    [InlineData(-1)]
    public void dado_um_novo_almoxarife_telefone_deve_ser_maior_que_0(int _telefone)
    {
-      Assert.Throws<ArgumentException>(() => new Almoxarife("nome", "123456", _telefone, 24198732))
+        Assert.Throws<ArgumentException>(() => AlmoxarifeBuilder.Novo().ComTelefone(_telefone).Criar())
             .Message.Equals("Telefone invalido");
    }
 
@@ -32,38 +36,35 @@ public class AlmoxarifeTest
    [InlineData(null)]
    public void dado_um_novo_almoxarife_senha_nao_deve_ser_vazia(string _senha)
    {
-      Assert.Throws<ArgumentException>(() =>
-         new Almoxarife("nome", _senha, 47000, 1231221321))
+        Assert.Throws<ArgumentException>(() => AlmoxarifeBuilder.Novo().ComSenha(_senha).Criar())
             .Message.Equals("Senha invalida");
    }
 
    [Theory]
    [InlineData("")]
    [InlineData(null)]
-   public void dado_um_novo_almoxarife_nome_nao_deve_ser_vazia(string _nome)
-   {
-      Assert.Throws<ArgumentException>(() =>
-         new Almoxarife(_nome, "24524", 47000, 1231221321))
+    public void dado_um_novo_almoxarife_nome_nao_deve_ser_vazia(string _nome)
+    {
+         Assert.Throws<ArgumentException>(() => AlmoxarifeBuilder.Novo().ComNome(_nome).Criar())
             .Message.Equals("Nome invalido");
-   }
+    }
 
-   [Fact]
-   public void dado_um_novo_almoxarife_valido_gerar_matricula()
-      => Assert.False(string.IsNullOrEmpty(_almoxarife.Matricula));
+    [Fact]
+    public void dado_um_novo_almoxarife_valido_gerar_matricula()
+        => Assert.False(string.IsNullOrEmpty(_almoxarife.Matricula));
 
    [Fact]
    public void dado_um_almoxarife_valido_matricula_deve_ter_8_caracter()
-   => Assert.Equal(8, _almoxarife.Matricula.Length);
+      => Assert.Equal(8, _almoxarife.Matricula.Length);
 
    // test pedidos
    [Fact]
    public void almoxarife_cadastrar_novo_produto()
    {
-      var produto = new Produto("nome", 123, "descrição");
-      var pedido = new Pedido("titulo", "descricao", new Colaborador("nome", "senha", 4748327, 817483, TipoColaborador.Externo, CargoColaborador.Suporte), produto);
-      _almoxarife.CadastrarNovoProduto(pedido);
+        var pedido = PedidoBuilder.Novo().Criar();
+        _almoxarife.CadastrarNovoProduto(pedido);
       
-      Assert.True(Estoque.Produtos.Count() != 0);
+        Assert.True(Estoque.Produtos.Count() != 0);
    }
 
    [Fact]
@@ -85,8 +86,7 @@ public class AlmoxarifeTest
    [InlineData(-1)]
    public void ao_adicionar_produto_unidades_deve_ser_maior_que_0(int _unidades)
    {
-      var produto = new Produto("nome", 123, "descricao");
-      Assert.Throws<ArgumentException>(() => _almoxarife.AdicionarUnidadesDeProduto(produto, _unidades))
+      Assert.Throws<ArgumentException>(() => _almoxarife.AdicionarUnidadesDeProduto(_produto, _unidades))
          .Message.Equals("Unidades deve ser maior que zero");
    }
 
@@ -96,17 +96,15 @@ public class AlmoxarifeTest
    [InlineData(-1)]
    public void ao_remover_produto_unidades_deve_ser_maior_que_0(int _unidades)
    {
-      var produto = new Produto("nome", 123, "descricao");
-      Assert.Throws<ArgumentException>(() => _almoxarife.RemoverUnidadesDeProduto(produto, _unidades))
+      Assert.Throws<ArgumentException>(() => _almoxarife.RemoverUnidadesDeProduto(_produto, _unidades))
          .Message.Equals("Unidades deve ser maior que zero");
    }
 
    [Fact]
    public void ao_remover_unidades_produto_deve_conter_mais_ou_igual_unidades()
    {
-      var produto = new Produto("nome", 123, "descricao");
-      produto.AdicionarUnidades(10);
-      Assert.Throws<ArgumentException>(() => _almoxarife.RemoverUnidadesDeProduto(produto, 11))
+      _produto.AdicionarUnidades(10);
+      Assert.Throws<ArgumentException>(() => _almoxarife.RemoverUnidadesDeProduto(_produto, 11))
          .Message.Equals("Não é possivel remover mais unidades do que existem");
    }
 
